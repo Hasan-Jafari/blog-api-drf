bf18d8a1254ff43e41d094e599b9504cb9bb6898
Hasan-Jafari <byhasanj@gmail.com>
2025-12-02 15:21:09 +0330

    Update phone_number_validator.py

--- core/validators/phone_number_validator.py
+++ core/validators/phone_number_validator.py
@@ -4,6 +4,6 @@ from django.core.exceptions import ValidationError
 
 
 def validate_phone_number(value):
-    if not re.match(r'^09\d{9}', value):
+    if not re.match(r'^09\d{9}$', value):
         raise ValidationError('Phone number is not True.')
     return value
