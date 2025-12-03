97d0b918668e99f463930539bb9fb37dab523a8e
Hasan-Jafari <byhasanj@gmail.com>
2025-12-03 15:55:37 +0330

    core models
    Add core blog models:
    
    - BaseModel with UUID and timestamps.
    
    - Category & Tag with auto-slug.
    
    - Post.
    
    - Comment.

---
+++ core/models/comment.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from slugify import slugify
+
+from .base import BaseModel
+from .post import Post
+
+
+
+class Comment(BaseModel):
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='post_com'
+    )
+    content = models.TextField()
+    approved = models.BooleanField(default=True)
+        
+    # def __str__(self):
+    #     return self.
+    
---
+++ core/models/post.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from slugify import slugify
+
+from .base import BaseModel
+from .category import Category
+from .tag import Tag
+
+
+
+class Status(models.TextChoices):
+    DRAFT = 'draft', 'Draft'
+    PUBLISHED = 'published', 'Published'
+    
+class Post(BaseModel):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    categories = models.ManyToManyField(
+        Category,
+        related_name='posts'
+    )
+    tag = models.ManyToManyField(
+        Tag,
+        related_name='posts'
+    )
+    status = models.CharField(
+        max_length=10,
+        choices=Status.choices,
+        default=Status.DRAFT
+    )
+    views_count =  models.PositiveIntegerField(default=0)
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title, allow_unicode=True)
+        super().save(*args, **kwargs)
+        
+    def __str__(self):
+        return self.title
